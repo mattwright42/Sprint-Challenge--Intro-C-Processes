bffff4ad976adc894a6c27dcc90d8e0d063c7608
Matthew Wright <matthewbwright@gmail.com>
2019-03-22 11:01:10 -0700

    completes MVP

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -7,19 +9,39 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *path;
+  if (argc > 1)
+  {
+    path = argv[1];
+  }
+  else
+  {
+    path = "./";
+  }
 
   // Open directory
   DIR *dir;
-  dir = opendir();
+  dir = opendir(path);
 
-  if (!dir)
+  // Check to see if the directory could not be opened.
+  if (dir == NULL)
   {
-    printf("Error: Could not open directory.");
+    printf("Error: Could not open directory. \n");
     exit(1);
   }
 
   // Repeatly read and print entries
-  readdir();
+  struct dirent *current_dir = readdir(dir);
+  while (current_dir)
+  {
+    struct stat buf;
+
+    printf("%s/%s", path, current_dir->d_name);
+    stat(path, &buf);
+
+    printf("%10lld %s\n", buf.st_size, current_dir->d_name);
+    current_dir = readdir(dir);
+  }
 
   // Close directory
   closedir(dir);
